d0a0f4e11841b2ecf42bddfabc6e72704c82c249
jsp-hashicorp <jsp@hashicorp.com>
2023-10-25 22:50:22 +0900

    fix variable

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = var.amiId
   instance_type = var.instanceType
 
   tags = {
--- variables.tf
+++ variables.tf
@@ -1,9 +1,24 @@
 variable "deployRegion" {
-  description = "AWS 사용 지역 설정"
+  description = <<-EOT
+   "
+   AWS 사용 지역 설정
+    - 서울 : ap-northeast-2
+    - 싱가폴 : ap-southeast-1 
+    - 도쿄 : ap-northeast-1
+   "
+   EOT
 }
 
 variable "instanceType" {
-  description = "EC2 인스턴스 타입 설정"
+  description = <<-EOT
+"
+EC2 인스턴스 타입 설정
+   1. t4.micro
+   2. t4.small
+   3. t4.medium
+   4. t4.large
+"
+EOT
 }
 
 variable "instanceName" {
@@ -12,6 +27,10 @@ variable "instanceName" {
 
 variable "amiId" {
   description = <<-EOT
-"사용할 머신 이미지 지정 : \n Amazon Linux 2023 ami-0f52ba4acb7f8f76a (64-bit (x86)) \n Red Hat Enterprise Linux 9 (HVM), SSD Volume Type ami-004b403708f61ecd8 (64-bit (x86)) \n Ubuntu Server 22.04 LTS (HVM), SSD Volume Type ami-086cae3329a3f7d75 (64-bit (x86))"
+"사용할 머신 이미지 지정 : 
+  Amazon Linux 2023 - ami-0f52ba4acb7f8f76a (64-bit (x86)) 
+  Red Hat Enterprise Linux 9 (HVM), SSD Volume Type - ami-004b403708f61ecd8 (64-bit (x86)) 
+  Ubuntu Server 22.04 LTS (HVM), SSD Volume Type - ami-086cae3329a3f7d75 (64-bit (x86))
+"
 EOT
  }
